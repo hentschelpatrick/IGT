e9dc9153fc76e8bca1762ed3e8a5b41c1eae884b
Soheb Abdi <sabdi@liveperson.com>
2018-12-07 16:11:26 +0100

    Add Bookflight.class

--- src/main/java/de/igt/controllers/FlightSegmentController.java
+++ src/main/java/de/igt/controllers/FlightSegmentController.java
@@ -177,6 +177,42 @@ public class FlightSegmentController {
         return cust;
     }
 
+    public List<FlightSegment> getAllFlightSegments() {
+
+        List<FlightSegment> cIDs = new ArrayList<>();
+        //List<String> returnList = new ArrayList<>();
+
+        try {
+            EntityManager em = emf.createEntityManager();
+
+            String queryString = new String("SELECT E FROM FlightSegment E");
+            Query q = em.createQuery(queryString);
+
+            //logger.info("\n\nGet all Flightsegment TA begins");
+            tm.setTransactionTimeout(Config.TRANSACTION_TIMEOUT);
+            tm.begin();
+
+            long queryStart = System.currentTimeMillis();
+
+            cIDs = q.getResultList();
+
+            /*
+            for (FlightSegment c : cIDs) {
+                returnList.add(c.toString());
+            }*/
+
+            long queryEnd = System.currentTimeMillis();
+            em.flush();
+            em.close();
+            tm.commit();
+            //logger.info("\n\nGet all Flightsegment TA ends");
+            long queryTime = queryEnd - queryStart;
+            //logger.info("\n\nFound " + cIDs.size() + " Flightsegment names in " + queryTime + " ms.\n\n");
+        } catch (NotSupportedException | SystemException | HeuristicRollbackException | HeuristicMixedException | RollbackException e) {
+            e.printStackTrace();
+        }
+        return cIDs;
+    }
 
     public List<String> getAllFlightSegmentNames() {
 
---
+++ src/main/java/de/igt/transactions/BookFlight.java
@@ -0,0 +1,88 @@
+package de.igt.transactions;
+
+import de.igt.controllers.AirportController;
+import de.igt.controllers.CustomerController;
+import de.igt.controllers.FlightController;
+import de.igt.controllers.FlightSegmentController;
+import de.igt.models.Airport;
+import de.igt.models.FlightSegment;
+import de.igt.models.Status;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+public class BookFlight {
+    public static void main(String[] args) {
+        try {
+            FlightController flightController = new FlightController();
+            AirportController airportController1 = new AirportController();
+            AirportController airportController2 = new AirportController();
+            FlightSegmentController flightSegmentController1 = new FlightSegmentController();
+            FlightSegmentController flightSegmentController2 = new FlightSegmentController();
+            CustomerController customerController = new CustomerController();
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Möchten Sie eine Flugbuchung durchführen?");
+            String temp = scanner.nextLine();
+
+            //Create a flight
+            flightController.createFlight("1", new Date(), new Date(), "TES", 450,
+                    250, 62, 31);
+
+            //Create two airports
+            airportController1.createAirport("FRA", "DE", "SFAKSD", 4, 5);
+            airportController2.createAirport("FRA2", "DE", "SDS", 5, 12);
+
+
+            Airport a1 = airportController1.getAirports("FRA");
+            Airport a2 = airportController2.getAirports("FRA2");
+
+
+            //Create first flightsegment
+            flightSegmentController1.createFlightsegment("Flight1", a1,
+                    a2, 19321);
+
+            //Create second flightsegment
+            //flightSegmentController2.createFlightsegment("Flight1", a2, a1, 19321);
+
+
+
+            System.out.println("Wo möchten Sie Ihre Reise starten?");
+            String departure_airport = scanner.nextLine();
+
+            System.out.println("Wo möchten Sie Ihre Reise beenden?");
+            String arrival_airport = scanner.nextLine();
+
+
+            List<FlightSegment> listOfFlightSegments = flightSegmentController1.getAllFlightSegments();
+            for(FlightSegment c : listOfFlightSegments) {
+                System.out.println(c.toString());
+            }
+
+            System.out.println("Wählen Sie eine der verfügbaren Flüge aus.");
+            int flug = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.println("Geben Sie Ihre Email an?");
+            String email = scanner.nextLine();
+            System.out.println("Geben Sie Ihren Vorname an?");
+            String vornamen = scanner.nextLine();
+            System.out.println("Geben Sie Ihren Nachname an?");
+            String nachnamen = scanner.nextLine();
+            System.out.println("Geben Sie Ihre Adresse an?");
+            String adresse = scanner.nextLine();
+            System.out.println("Geben Sie Ihr Alter an?");
+            int alter = scanner.nextInt();
+
+
+            customerController.createCustomer(email, vornamen, nachnamen,
+                    adresse,alter, "DE", 0, 0, Status.NONE);
+
+
+
+        } finally {
+            System.out.println("Finished");
+        }
+    }
+}
